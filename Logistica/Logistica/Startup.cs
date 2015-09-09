e6afb5e75fa764a69d4644fd7017b8c47d3d19e5
Cesar Patino <caparturo@outlook.com>
2015-09-08 23:25:03 -0500

    inicio

---
+++ Logistica/Logistica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Logistica.Startup))]
+namespace Logistica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
