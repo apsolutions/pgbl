fb22beb15af02f1aacb949abd3cbb3ed045d911d
Cesar Patino <caparturo@outlook.com>
2015-09-13 21:47:56 -0500

    Administracion de roles terminadad, pendiente de agregar el nivel de uso

--- Logistica/Logistica/Controllers/RolesController.cs
+++ Logistica/Logistica/Controllers/RolesController.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Logistica.Models;
+
 
 namespace Logistica.Controllers
 {
+    [Authorize]
     public class RolesController : Controller
     {
+
+        ApplicationDbContext context = new ApplicationDbContext();
+
         // GET: Roles
         public ActionResult Index()
         {
-            return View();
-        }
-
-        // GET: Roles/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
+            var roles = context.Roles.ToList();
+            return View(roles);
         }
 
         // GET: Roles/Create
@@ -34,6 +38,12 @@ namespace Logistica.Controllers
             {
                 // TODO: Add insert logic here
 
+                context.Roles.Add(new IdentityRole()
+                {
+                    Name = collection["Name"]
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Rol creado satisfactoriamente!";
                 return RedirectToAction("Index");
             }
             catch
@@ -43,18 +53,31 @@ namespace Logistica.Controllers
         }
 
         // GET: Roles/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
-            return View();
+            if (id.Equals(string.Empty))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var thisRole = context.Roles.Find(id);
+
+            if (thisRole == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(thisRole);
         }
 
         // POST: Roles/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(IdentityRole role)
         {
             try
             {
-                // TODO: Add update logic here
+                context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -64,20 +87,15 @@ namespace Logistica.Controllers
             }
         }
 
-        // GET: Roles/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
         // POST: Roles/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(string id)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                var thisRole = context.Roles.Find(id);
+                context.Roles.Remove(thisRole);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
---
+++ Logistica/Logistica/Models/RolesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Logistica.Models
+{
+    public class RolesViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "El campo rol es requerido.")]
+        [Display(Name = "Nombre del rol")]
+        public string Name { get; set; }
+
+    }
+}
