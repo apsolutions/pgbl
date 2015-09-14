8f9e6976b22272772b058bc60c4e9ffd7d38dbb5
Aldo Afranchi <afranchimurillo@hotmail.com>
2015-09-14 02:14:16 -0500

    Se agrego model y create en el controller comunidades

---
+++ Logistica/Logistica/Models/ComunidadesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logistica.Models
+{
+    public class ComunidadesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int ComMembers { get; set; }
+        public bool Disable { get; set; } 
+    }
+}
