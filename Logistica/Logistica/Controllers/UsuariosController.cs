44d12722c82f06e5ff0a2bd874631ac392a28410
Cesar Patino <caparturo@outlook.com>
2015-09-19 21:22:38 -0500

    Correcion de algunos terminos, administracion de usuarios casi completa

--- Logistica/Logistica/Controllers/UsuariosController.cs
+++ Logistica/Logistica/Controllers/UsuariosController.cs
@@ -5,23 +5,50 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Logistica.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace Logistica.Controllers
 {
-
+    [Authorize]
     public class UsuariosController : Controller
     {
         private ApplicationDbContext identity = new ApplicationDbContext();
+
+        private ApplicationUserManager _userManager;
+
+        public UsuariosController()
+        {
+        }
+
+        public UsuariosController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Usuarios
         public ActionResult Index()
         {
-            var usuarios = identity.Users.ToList();
-            return View(usuarios);
-        }
-
-        // GET: Usuarios/Details/5
-        public ActionResult Details(int id)
-        {
+            ViewBag.usuarios = (from u in identity.Users
+                            select new
+                            {
+                                u.Id,
+                                u.UserName,
+                                Seleccionado = false
+                            }).ToList();
             return View();
         }
 
@@ -33,61 +60,169 @@ namespace Logistica.Controllers
 
         // POST: Usuarios/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(UsuarioViewModel collection)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+                var user = new ApplicationUser { UserName = collection.UserName, Email = collection.UserName };
+                var result = await UserManager.CreateAsync(user, "GlobalPanama01!");
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                ViewBag.result = result;
+            }
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            // If we got this far, something failed, redisplay form
+            return View(collection);
         }
 
         // GET: Usuarios/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
-            return View();
+            if (id.Equals(string.Empty))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = identity.Users.Find(id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            UsuarioViewModel usuario = new UsuarioViewModel() { Id = user.Id, UserName = user.UserName };
+            return View(usuario);
         }
 
         // POST: Usuarios/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(UsuarioViewModel collection)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                try
+                {
+                    // TODO: Add update logic here
+                    var usuario = identity.Users.Find(collection.Id);
+                    usuario.UserName = collection.UserName;
+                    usuario.Email = collection.UserName;
+                    identity.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View(collection);
+                }
             }
-            catch
-            {
-                return View();
-            }
-        }
 
-        // GET: Usuarios/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
+            return View(collection);
+            
         }
 
         // POST: Usuarios/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(string id)
         {
+            if (id.Equals(string.Empty))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = identity.Users.Find(id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
             try
             {
-                // TODO: Add delete logic here
-
+                // TODO: Add update logic here
+                identity.Users.Remove(user);
+                identity.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
                 return View();
+            }            
+        }
+
+        public ActionResult Roles(string id)
+        {
+            if (id == null || id.Equals(string.Empty))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ViewBag.rolesUsuario = (from r in identity.Roles
+                                where r.Users.Where(u => u.UserId.Equals(id, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault() != null
+                                select new
+                                {
+                                    r.Id,
+                                    r.Name,
+                                    Seleccionado = false
+                                }).ToList();
+            
+            ViewBag.rolesDisponibles = (from r in identity.Roles
+                                        where r.Users.Where(u => u.UserId.Equals(id, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault() == null
+                                        select new
+                                        {
+                                            r.Id,
+                                            r.Name,
+                                            Seleccionado = false
+                                        }).ToList();
+            ViewBag.usuario = (from u in identity.Users
+                               where u.Id.Equals(id, StringComparison.CurrentCultureIgnoreCase)
+                               select new
+                               {
+                                   u.Id,
+                                   u.UserName
+                               }).ToList();
+            if (ViewBag.rolesUsuario == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public bool SalvarRoles(string userName, List<RolesViewModel> roles)
+        {
+            try
+            {
+                var usuario = identity.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.CurrentCultureIgnoreCase));
+                var rolesUsuario = usuario.Roles.ToList();
+
+                if (rolesUsuario.Count != 0)
+                {
+                    foreach (var rol in rolesUsuario)
+                    {
+                        usuario.Roles.Remove(rol);
+                    }
+                }
+
+                if (roles != null && roles.Count != 0)
+                {
+                    foreach (var rol in roles)
+                    {
+                        usuario.Roles.Add(new IdentityUserRole()
+                        {
+                            RoleId = rol.Id
+                        });
+                    }
+                }
+
+                identity.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
--- Logistica/Logistica/Models/UsuarioViewModel.cs
+++ Logistica/Logistica/Models/UsuarioViewModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Logistica.Models
 {
     public class UsuarioViewModel
     {
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "El campo usuario es requerido.")]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
     }
 }
